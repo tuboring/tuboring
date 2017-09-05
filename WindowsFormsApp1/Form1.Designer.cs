967105daec2dca4567f2a56ff9d3a70dc5a357e5
sasha <sasha@ALEXANDER>
2017-09-05 18:07:11 +0500

    05.09 1806

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -37,9 +37,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(45, 145);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.Size = new System.Drawing.Size(172, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "EZE EZE REAL TOK";
+            this.label1.Text = "EZE EZE REAL TOK think about it";
             // 
             // Form1
             // 
