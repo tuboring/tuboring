c0c364633cdc55b48b3a956fc526908fec6fe0e6
sasha <sasha@ALEXANDER>
2017-09-24 23:47:14 +0500

    6

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(45, 145);
+            this.label1.Location = new System.Drawing.Point(162, 104);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 0;
