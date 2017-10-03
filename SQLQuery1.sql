77e99a75a87c9520705efb5b5b3bb64f40eedc9c
Алина Сафина <gold_rose77@mail.ru>
2017-10-03 14:39:41 +0500

    z

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,1 +1,8 @@
-﻿SELECT * FROM [User] join [Role] on [User].[RoleId]=[Role].[RoleId] WHERE [Email] = 'a.ashton@saucedout.com'
+﻿CREATE TABLE [dbo].[RegOnMatch] (
+    [IdReg]                INT           IDENTITY (1, 1) NOT NULL,
+    [EmailSportsmen]       NVARCHAR (100) NOT NULL,
+    [EventId]              NCHAR(6)           NOT NULL,
+    [CityName]             NCHAR (50)    NOT NULL,
+    [RegistrationStatucId] INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([IdReg] ASC)
+);
